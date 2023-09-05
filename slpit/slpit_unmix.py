626c7b4bcbea7af333e550ceba6cd7118da59e20
fotxoa <fochoa1@g.ucla.edu>
2023-09-05 13:10:15 -0700

    unmix calls

--- slpit/slpit_unmix.py
+++ slpit/slpit_unmix.py
@@ -9,6 +9,7 @@ from utils.text_guide import cursor_print
 from utils.spectra_utils import spectra
 import geopandas as gp
 from datetime import datetime
+import time
 
 class unmix_runs:
     def __init__(self, base_directory: str,  dry_run=False):
@@ -22,6 +23,7 @@ class unmix_runs:
         # simulation parameters for spatial and hypertrace unmix
         self.optimal_parameters_sma = ['--num_endmembers 20', '--n_mc 25', '--normalization brightness']
         self.optimal_parameters_mesma = ['--max_combinations 1000', '--n_mc 25', '--normalization brightness']
+        self.non_opt_mesma = ['--max_combinations 1000', '--n_mc 1', '--normalization brightness']
 
         # load wavelengths
         self.wvls, self.fwhm = spectra.load_wavelengths(sensor='emit')
@@ -54,44 +56,50 @@ class unmix_runs:
         for index, row in df.iterrows():
             plot = row['Name']
 
-            image_acquisition_time_ipad = row['EMIT Overp']
-            input_datetime = datetime.strptime(image_acquisition_time_ipad, "%b %d, %Y at %I:%M:%S %p")
-            emit_filetime = input_datetime.strftime("%Y%m%dT%H%M")
+            emit_filetime = row['EMIT Date']
+            #input_datetime = datetime.strptime(image_acquisition_time_ipad, "%b %d, %Y at %I:%M:%S %p")
+            #emit_filetime = input_datetime.strftime("%Y%m%dT%H%M")
 
-            reflectance_img_emit = glob(os.path.join(self.gis_directory, 'emit-data-clip', f'*{plot.replace(" ", "")}_RFL_{emit_filetime}*[!.xml][!.hdr]'))
-            reflectance_uncer_img_emit = glob(os.path.join(self.gis_directory, 'emit-data-clip',f'*{plot.replace(" ", "")}_RFLUNCERT_{emit_filetime}*[!.xml][!.hdr]'))
+            reflectance_img_emit = glob(os.path.join(self.gis_directory, 'emit-data-clip', f'*{plot.replace(" ", "")}_RFL_{emit_filetime}'))
+            reflectance_uncer_img_emit = glob(os.path.join(self.gis_directory, 'emit-data-clip',f'*{plot.replace(" ", "")}_RFLUNCERT_{emit_filetime}'))
             em_local = os.path.join(self.spectral_em_directory, plot.replace("SPEC", 'Spectral').replace(" ", "") + '-emit.csv')
             asd_reflectance = glob(os.path.join(self.spectral_transect_directory, f'*{plot.replace("SPEC", "Spectral").replace(" ", "")}'))
+            
             if os.path.isfile(em_local):
                 if mode == 'mesma':
-                    simulation_parameters = self.optimal_parameters_mesma
+                    simulation_parameters = [self.optimal_parameters_mesma, self.non_opt_mesma] 
                 else:
-                    simulation_parameters = self.optimal_parameters_sma
+                    simulation_parameters = [self.optimal_parameters_sma]
 
                 output_dest = os.path.join(self.output_directory, mode)
+                
+                for i in simulation_parameters:
+                    # unmix asd with local library
+                    out_param_string = " ".join(i)
+                    out_param_name = plot.replace(" ", "") + "___" + out_param_string.replace('--','').replace('_', '-').replace(' ', '_')
 
-                # unmix asd with local library
-                call_unmix(mode=mode, dry_run=self.dry_run, reflectance_file=asd_reflectance[0], em_file=em_local,
-                           parameters=simulation_parameters, output_dest=os.path.join(output_dest, 'asd-local_' + plot.replace(" ", "")),
+
+                    call_unmix(mode=mode, dry_run=self.dry_run, reflectance_file=asd_reflectance[0], em_file=em_local,
+                           parameters=i, output_dest=os.path.join(output_dest, 'asd-local___' + out_param_name),
                            scale=self.scale,  spectra_starting_column=self.spectra_starting_column_local)
 
-                # unmix asd with global library
-                call_unmix(mode=mode, dry_run=self.dry_run, reflectance_file=asd_reflectance[0], em_file=self.emit_global,
-                           parameters=simulation_parameters, output_dest=os.path.join(output_dest, 'asd-global_' + plot.replace(" ", "")),
+                    # unmix asd with global library
+                    call_unmix(mode=mode, dry_run=self.dry_run, reflectance_file=asd_reflectance[0], em_file=self.emit_global,
+                           parameters=i, output_dest=os.path.join(output_dest, 'asd-global___' + out_param_name),
                            scale=self.scale,  spectra_starting_column=self.spectra_starting_column_global)
 
-                # emit pixels unmixed with local em
-                call_unmix(mode=mode, dry_run=self.dry_run, reflectance_file=reflectance_img_emit[0], em_file=em_local,
-                           parameters=simulation_parameters, output_dest=os.path.join(output_dest, 'emit-local_' + plot.replace(" ", "")),
+                    # emit pixels unmixed with local em
+                    call_unmix(mode=mode, dry_run=self.dry_run, reflectance_file=reflectance_img_emit[0], em_file=em_local,
+                           parameters=i, output_dest=os.path.join(output_dest, 'emit-local___' + out_param_name),
                            scale=self.scale, uncertainty_file=reflectance_uncer_img_emit[0],
                            spectra_starting_column=self.spectra_starting_column_local)
 
-                # emit pixels unmixed with global
-                call_unmix(mode=mode, dry_run=self.dry_run, reflectance_file=reflectance_img_emit[0], em_file=self.emit_global,
-                           parameters=simulation_parameters, output_dest=os.path.join(output_dest, 'emit-global_' + plot.replace(" ", "")),
+                    # emit pixels unmixed with global
+                    call_unmix(mode=mode, dry_run=self.dry_run, reflectance_file=reflectance_img_emit[0], em_file=self.emit_global,
+                           parameters=i, output_dest=os.path.join(output_dest, 'emit-global___' + out_param_name),
                            scale=self.scale, uncertainty_file=reflectance_uncer_img_emit[0],
                            spectra_starting_column=self.spectra_starting_column_global)
-
+                
             else:
                 print(em_local, " does not exist.")
 
