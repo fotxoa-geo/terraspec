d897561d3851d41d751b34302f6da7901a6fa1e2
fotxoa <fochoa1@g.ucla.edu>
2023-09-30 17:26:58 -0600

    minor fixes to sh script

--- tetracorder/tetracorder.sh
+++ tetracorder/tetracorder.sh
@@ -6,16 +6,16 @@ filebase=`basename ${rfl_file}`
 
 tmp_rfl_path=/tmp/`basename ${rfl_file}`
 tmp_tetra_path=/tmp/${filebase}_tetra_output
-out_tetra_path=${out_base}_tetra
-out_min_path=${out_base}_min
-out_minunc_path=${out_base}_minunc
-out_abun_path=${out_base}_abun
+out_tetra_path=${out_base}${filebase}_tetra
+out_min_path=${out_base}${filebase}_min
+out_minunc_path=${out_base}${filebase}_minunc
+out_abun_path=${out_base}${filebase}_abun
 
 echo $tmp_tetra_path
 echo $out_tetra_path
 
-cp ${rfl_path_base} $tmp_rfl_path
-cp ${rfl_path_base}.hdr ${tmp_rfl_path}.hdr
+cp ${rfl_file} $tmp_rfl_path
+cp ${rfl_file}.hdr ${tmp_rfl_path}.hdr
 
 export SP_LOCAL=/beegfs/store/shared/specpr
 export SP_BIN="${SP_LOCAL}/bin"
@@ -29,11 +29,11 @@ cd $tmp_tetra_path
 ${tmp_tetra_path}/cmd.runtet cube $tmp_rfl_path band 20 gif
 cd $cpwd
 
-rm $tmp_rfl_path
-rm ${tmp_rfl_path}.hdr
-
 python /beegfs/scratch/brodrick/emit/emit-sds-l2b/group_aggregator.py $tmp_tetra_path /beegfs/scratch/brodrick/emit/emit-sds-l2b/mineral_grouping_matrix_20230503.csv $out_min_path $out_minunc_path --reflectance_file $tmp_rfl_path --reflectance_uncertainty_file $tmp_rfl_path --reference_library /beegfs/store/shared/tetracorder_libraries/s06emitd_envi --research_library /beegfs/store/shared/tetracorder_libraries/r06emitd_envi
 
 python /beegfs/scratch/brodrick/emit/emit-sds-l2b/abundance_from_min.py $out_abun_path $out_min_path --mineral_groupings_matrix /beegfs/scratch/brodrick/emit/emit-sds-l2b/data/mineral_grouping_matrix_20230503.csv 
+
+rm $tmp_rfl_path
+rm ${tmp_rfl_path}.hdr
 
 rm -rf $tmp_tetra_path
