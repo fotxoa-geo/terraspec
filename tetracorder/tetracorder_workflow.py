7676090f4610272c4b6c7a8710fa5b1b3a99a44c
fotxoa <fochoa1@g.ucla.edu>
2023-10-03 16:06:03 -0600

    added interactive menu and exclusions

--- tetracorder/tetracorder_workflow.py
+++ tetracorder/tetracorder_workflow.py
@@ -1,21 +1,50 @@
+import os.path
+import subprocess
 from utils.text_guide import cursor_print, query_tetracorder_mode
 from tetracorder.build_tetracorder import run_tetracorder_build
 from tetracorder.figures import run_figure_workflow
+from glob import glob
+
+def display_tetracorder_menu():
+    msg = f"You have entered Tetracorder mode! " \
+          f"\nThere are various options to chose from: "
+    cursor_print(msg)
+    print("Welcome to the Tetracorder Mode....")
+    print("A... Augment data files")
+    print("B... Run Tetracorder")
+    print("C... Figures")
+    print("D... Exit")
 
 
 def run_tetracorder_workflow(base_directory:str, sensor:str):
-    msg = f"You have entered Tetracorder mode! " \
-          f"\nTetracorder is not part of this package." \
-          f"\nThere are two options to chose from: " \
-          f"\n\t build, figures"
+    while True:
+        display_tetracorder_menu()
+        user_input = input('\nPlease indicate the desired mode: ').upper()
 
-    cursor_print(msg)
-    user_input = query_tetracorder_mode('\nPlease indicate the desired mode: ', default="yes")
+        # run build workflow
+        if user_input == 'A':
+            run_tetracorder_build(base_directory, sensor=sensor)
 
-    # run build workflow
-    if user_input == 'build':
-        run_tetracorder_build(base_directory, sensor=sensor)
+        # run Tetracorder workflow
+        elif user_input == 'B':
+            augmented_files = glob(os.path.join(base_directory, 'output', 'augmented', '*'))
+            output = os.path.join(base_directory, 'output', 'spectral_abundance')
+            exclude = ['.hdr', '.aux', '.xml']
 
-    # run figure workflow
-    if user_input == 'figures':
-        run_figure_workflow(base_directory)
+            for i in augmented_files:
+                if os.path.splitext(i)[1] in exclude:
+                    pass
+                else:
+                    base_call = f'sh tetracorder/tetracorder.sh {i} {output}'
+                    sbatch_cmd = f'srun -N 1 -C 1 --mem=40G {base_call}'
+                    subprocess.call(sbatch_cmd, shell=True)
+
+        # run figure workflow
+        elif user_input == 'C':
+            run_figure_workflow(base_directory)
+
+        elif user_input == "D":
+            print("Returning to main menu.")
+            break
+        else:
+            print("Invalid choice. Please choose a valid option.")
