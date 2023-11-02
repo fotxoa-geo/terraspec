e5b569d3f02555713e41083f583b7672c33fda5f
fotxoa <fochoa1@g.ucla.edu>
2023-11-02 12:29:56 -0700

    added various call functions for a more general unmix call

--- utils/unmix_utils.py
+++ utils/unmix_utils.py
@@ -27,12 +27,14 @@ def hypertrace_meta(reflectance_file):
     aod = os.path.abspath(reflectance_file).split('/')[-3].split("__")[0].split("_")[1]
     h2o = os.path.abspath(reflectance_file).split('/')[-3].split("__")[1].split("_")[1]
 
-    basename = [atmosphere, altitude, doy, long, lat, azimuth, sensor_zenith, time, elev, aod, h2o]
-    basename = "_".join(basename)
+    basename = ['atm', doy, azimuth, sensor_zenith, time, elev, aod, h2o]
+    basename = "_".join(basename).replace('.', '-')
 
     return basename
+
 def create_uncertainty(uncertainty_file: str, wvls):
     output = os.path.join(os.path.dirname(uncertainty_file), 'reflectance_uncertainty.hdr')
+    
     if os.path.isfile(output):
         pass
     else:
@@ -67,14 +69,16 @@ def call_unmix(mode: str, reflectance_file: str, em_file: str, dry_run: bool, pa
 
 
 def call_hypertrace_unmix(mode: str, reflectance_file: str, em_file: str, dry_run: bool, parameters: list, output_dest: str,
-                          scale: str, spectra_starting_column: str):
+                          scale: str, spectra_starting_column: str, uncertainty_file:str):
 
-    # path to reflectance uncertainty file
-    uncertainty_file = os.path.join(os.path.dirname(reflectance_file), "reflectance_uncertainty")
+    create_directory(os.path.join(output_dest, mode))
+    create_directory(os.path.join(output_dest, mode, 'outlogs'))
+    outlog_name = os.path.join(output_dest, mode, 'outlogs', os.path.basename(reflectance_file) + '.out')
+    output_dest = os.path.join(output_dest, mode, os.path.basename(reflectance_file))
 
     base_call = f'julia -p {n_cores} ~/EMIT/SpectralUnmixing/unmix.jl {reflectance_file} {em_file} ' \
                 f'{level_arg} {output_dest} --mode {mode} --spectral_starting_column {spectra_starting_column} --refl_scale {scale} --reflectance_uncertainty_file {uncertainty_file} ' \
                 f'{" ".join(parameters)}'
 
-    execute_call(['sbatch', '-N', "1", '-c', n_cores, '--mem', "180G", '--wrap', f'{base_call}'], dry_run)
+    execute_call(['sbatch', '-N', "1", '-c', n_cores, '--mem', "180G", '--output', outlog_name,'--wrap', f'{base_call}'], dry_run)
 
