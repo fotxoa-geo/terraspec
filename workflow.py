a88460079c74b1d2045f420baae38a05cefc82d0
fotxoa <fochoa1@g.ucla.edu>
2024-03-05 10:26:46 -0800

    io mode added

--- workflow.py
+++ workflow.py
@@ -17,12 +17,13 @@ def display_menu():
 def main():
     parser = argparse.ArgumentParser(description='Run Terraspec')
     parser.add_argument('-bd', '--base_root_directory', type=str, default='~', help='Specify project directory (e.g., where outputs will save')
-    parser.add_argument('-dry', '--dry_run', type=bool, help=' Set the dry run parameter to True to print unmix call',
-                        default=False)
+    parser.add_argument('-dry', '--dry_run', type=bool, help=' Set the dry run parameter to True to print unmix call', default=False)
     parser.add_argument('-lvl', '--level', type=str, help='level of classification to use', default='level_1')
     parser.add_argument('-sns', '--sensor', type=str, help='specify sensor to use', default='emit', choices=['emit', 'aviris-ng'])
+    parser.add_argument('-io', '--io_bug', action='store_false', help='IO Bug found in simulation', default=True)
+    
     args = parser.parse_args()
-
+    
     base_directory = os.path.join(args.base_root_directory, 'terraspec')
 
     create_directory(base_directory)
@@ -37,7 +38,7 @@ def main():
         choice = input("Enter desired mode: ").upper()
 
         if choice == "A":
-            run_sim_workflow(os.path.join(base_directory, 'simulation'), dry_run=args.dry_run)
+            run_sim_workflow(os.path.join(base_directory, 'simulation'), dry_run=args.dry_run, io_bug=args.io_bug)
 
         elif choice == 'B':
             run_slpit_workflow(os.path.join(base_directory, 'slpit'), dry_run=args.dry_run, sensor=args.sensor)
