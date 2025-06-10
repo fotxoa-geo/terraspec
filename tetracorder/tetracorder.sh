fbec656090b3ccfa82d1400777b2af68a85f3829
fochoa <fochoa1@g.ucla.edu>
2025-06-10 13:49:01 -0700

    enabled tetracorder mode

--- tetracorder/tetracorder.sh
+++ tetracorder/tetracorder.sh
@@ -34,7 +34,7 @@ cd $cpwd
 
 cp ${tmp_tetra_path} ${out_tetra_path} -r
 
-python /store/brodrick/repos/emit-sds-l2b/group_aggregator.py $out_tetra_path /store/fochoa/terraspec/utils/tetracorder/mineral_grouping_matrix_20230503.csv $out_min_path $out_minunc_path --reflectance_file $tmp_rfl_path --reflectance_uncertainty_file $tmp_rfl_path --reference_library /store/shared/tetracorder_libraries/s06emitd_envi --research_library /store/shared/tetracorder_libraries/r06emitd_envi --expert_system_file cmd.lib.setup.t5.27d1
+python /store/brodrick/repos/emit-sds-l2b/group_aggregator.py $out_tetra_path /store/fochoa/terraspec/utils/tetracorder/mineral_grouping_matrix_20230503.csv $out_min_path $out_minunc_path --reflectance_file $tmp_rfl_path --reflectance_uncertainty_file $tmp_rfl_path --reference_library /store/shared/tetracorder_libraries/s06emitd_envi --research_library /store/shared/tetracorder_libraries/r06emitd_envi --expert_system_file cmd.lib.setup.t5.27d1 --calculate_uncertainty 
 
 #python /beegfs/scratch/brodrick/emit/emit-sds-l2b/abundance_from_min.py $out_abun_path $out_min_path --mineral_groupings_matrix /beegfs/scratch/brodrick/emit/emit-sds-l2b/data/mineral_grouping_matrix_20230503.csv
 #rm $tmp_rfl_path
