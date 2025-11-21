aa1c4ca472d87ffcb3206b4854c6c274db392929
fochoa <fochoa1@ndc.nasa.gov>
2025-11-21 15:19:03 -0800

    changes to simulation

--- workflow.py
+++ workflow.py
@@ -21,18 +21,22 @@ def display_menu():
 
 def main():
     parser = argparse.ArgumentParser(description='Run Terraspec')
-    parser.add_argument('-bd', '--base_root_directory', type=str, default='/home/fochoa/store/terraspec_output/', help='Specify project directory (e.g., where outputs will save')
+    parser.add_argument('-bd', '--base_root_directory', type=str, default='.', help='Specify project directory (e.g., where outputs will save')
     parser.add_argument('-dry', '--dry_run', type=bool, help=' Set the dry run parameter to True to print unmix call', default=False)
     parser.add_argument('-lvl', '--level', type=str, help='level of classification to use', default='level_1')
     parser.add_argument('-sns', '--sensor', type=str, help='specify sensor to use', default='emit',
                         choices=['emit', 'aviris_ng', 'modis', 'hlss30', 'neon', 'aviris-classic', 'mavic3m', 'av3'])
-    parser.add_argument('-io', '--io_bug', action='store_false', help='IO Bug found in simulation', default=True)
+    #parser.add_argument('-io', '--io_bug', action='store_false', help='IO Bug found in simulation', default=True)
     parser.add_argument('-new_sim_bundles', '--new_simulation_bundles', type=bool, help='Run entire new simulation bundles calculation', default=False)
-    parser.add_argument('-num_of_bund', '--number_of_bundles', type=str, help='number of spectal bundles to use', default='100000')
+    parser.add_argument('-num_of_bund', '--number_of_bundles', type=str, help='number of spectral bundles to use', default='100000')
     parser.add_argument('-dis_geo', '--disable_geo_filter', action='store_false', help='Apply EMIT mask and selection from Ochoa et al 2025', default=True)
+    parser.add_argument('-norm', '--normalization', type=bool, help='Brightness normalization enabled for PC!', default=True)
+    parser.add_argument('-spec_start_col', '--spectra_start_col', type=str, help='Spectra starting column', default='7')
+    parser.add_argument('-n_cores', '--number_of_cores', type=str, help='Number of cores to use', default='40')
+
     args = parser.parse_args()
     
-    base_directory = os.path.join(args.base_root_directory, 'terraspec')
+    base_directory = os.path.join(args.base_root_directory, 'terraspec_output')
 
     create_directory(base_directory)
     subprocess.call(['python', 'utils/create_tree.py', '-bd', base_directory])
@@ -46,8 +50,11 @@ def main():
         choice = input("Enter desired mode: ").upper()
 
         if choice == "A":
-            run_sim_workflow(os.path.join(base_directory, 'simulation'), dry_run=args.dry_run, io_bug=args.io_bug,
-                             sensor=args.sensor, level=args.level, geo_filter=args.disable_geo_filter)
+            run_sim_workflow(os.path.join(base_directory, 'simulation'), dry_run=args.dry_run,
+                             sensor=args.sensor, level=args.level, geo_filter=args.disable_geo_filter,
+                             new_simulation_bundles=args.new_simulation_bundles, spectral_bundles=int(args.number_of_bundles),
+                             normalization=args.normalization, spectra_starting_column=int(args.spectra_start_col),
+                             n_cores=args.number_of_cores)
 
         elif choice == 'B':
             run_slpit_workflow(os.path.join(base_directory, 'slpit'), dry_run=args.dry_run, sensor=args.sensor)
