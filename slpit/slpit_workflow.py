ec40571a34ed7748b037ae2612edd96f41d83c70
Francisco Ochoa <francisco.ochoa@jpl.nasa.gov>
2025-12-18 16:03:44 -0800

    new calls for slpit unmix to .sh file; does tetracorder; augment moved to script for multi-use

--- slpit/slpit_workflow.py
+++ slpit/slpit_workflow.py
@@ -4,9 +4,10 @@ from utils.slpit_download import run_download_emit, run_dowloand_slpit, sync_gdr
 from slpit.geoprocess import run_geoprocess_utils
 from slpit.build_slpit import run_build_workflow
 from slpit.figures import run_figures
-from slpit.slpit_unmix import run_slipt_unmix
 from slpit.slpit_tables import run_tables
-
+import pandas as pd
+import geopandas as gp
+from glob import glob
 
 def display_slpit_menu():
     msg = f"You have entered SLPIT mode! " \
@@ -51,15 +52,39 @@ def run_slpit_workflow(base_directory:str, dry_run, sensor):
         elif user_input == 'C':
             run_build_workflow(base_directory=base_directory, sensor=sensor)
 
-        # run the geoprocess on the emit imagery
-        #elif user_input == 'D':
-        #    run_geoprocess_utils(base_directory=base_directory, dry_run=dry_run)
-
-        elif user_input =='F':
-            sync_extracts(base_directory, project='emit')
+        #elif user_input =='F':
+        #    sync_extracts(base_directory, project='emit')
 
         # run unmixing code
         elif user_input == 'G':
+            # load shapefile
+            df = pd.DataFrame(gp.read_file(os.path.join('gis', "Observation.json")))
+            df = df.sort_values('Name')
+
+            # create directory
+            count = 0
+
+            for index, row in df.iterrows():
+                plot = row['Name']
+
+                emit_filetime = row['EMIT DATE']
+                reflectance_img_emit = glob(os.path.join(self.gis_directory, 'emit-data-clip', f'*{plot.replace(" ", "")}_RFL_{emit_filetime}'))
+                reflectance_uncer_img_emit = glob(os.path.join(self.gis_directory, 'emit-data-clip',
+                                                               f'*{plot.replace(" ", "")}_RFLUNCERT_{emit_filetime}'))
+                em_local = os.path.join(self.spectral_em_directory,
+                                        plot.replace("SPEC", 'Spectral').replace(" ", "") + '-emit.csv')
+                asd_reflectance = glob(os.path.join(self.spectral_transect_directory,
+                                                    f'*{plot.replace("SPEC", "Spectral").replace(" ", "")}'))
+
+                if os.path.isfile(em_local):
+
+                    output_dest = os.path.join(self.output_directory, mode)
+                    # unmix asd with local library
+
+                else:
+                    print(em_local, " does not exist.")
+
+
             run_slipt_unmix(base_directory=base_directory, dry_run=dry_run)
 
         # run the figure set
