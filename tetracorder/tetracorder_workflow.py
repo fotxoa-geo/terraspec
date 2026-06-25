e5e89890396af74d6980a10f0f8c7745cc584fbc
fochoa <fochoa1@g.ucla.edu>
2026-06-25 10:06:22 -0700

    changes to workflow

--- tetracorder/tetracorder_workflow.py
+++ tetracorder/tetracorder_workflow.py
@@ -16,14 +16,14 @@ def display_tetracorder_menu():
     print("C... Exit")
 
 
-def run_tetracorder_workflow(base_directory:str, sensor:str, dry_run:bool, spectral_bundles:int):
+def run_tetracorder_workflow(base_directory:str, sensor:str, dry_run:bool, spectral_bundles:int, partition:str):
     while True:
         display_tetracorder_menu()
         user_input = input('\nPlease indicate the desired mode: ').upper()
 
         # run build workflow
         if user_input == 'A':
-            run_tetracorder_build(base_directory, sensor=sensor, dry_run=dry_run, spectral_bundles=spectral_bundles)
+            run_tetracorder_build(base_directory, sensor=sensor, dry_run=dry_run, spectral_bundles=spectral_bundles, partition=partition)
 
         # run figure workflow
         elif user_input == 'B':
